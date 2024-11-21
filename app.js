b34c3527ee7cde969dd9dfa9a43bff24e0a6916d
aliyan310 <aliyanfarooqiworld@gmail.com>
2024-11-22 03:22:23 +0500

    done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var input = document.querySelector(".form-control");
-var ol = document.querySelector("ol");
+var ul = document.querySelector("ul");
 var globalArr = [];
 
 
@@ -8,14 +8,14 @@ var globalArr = [];
 function render(){
     for(var i=0;i<globalArr.length;i++){
         console.log(`${globalArr[i]}`);
-        ol.innerHTML += `<li>${globalArr[i]}
+        ul.innerHTML += `<li>${globalArr[i]}
         <div><button class= "btn-1" onclick="todoDelete(${i})">Delete</button>
         <button class= "btn-2" onclick="todoEdited(${i})">Edit</button></li><br></div>`;
 }
 }
 
 function addTodo(){
-    ol.innerHTML = "";
+    ul.innerHTML = "";
     console.log(input.value);
     globalArr.push(input.value);
     console.log(globalArr);
@@ -25,14 +25,14 @@ function addTodo(){
 
 function todoDelete(index){
     globalArr.splice(index , 1);
-    ol.innerHTML = "";
+    ul.innerHTML = "";
    render()
     }
 
 function todoEdited(index){
     var editValue = prompt("Enter the value here", globalArr[index])
     globalArr.splice(index,1,editValue)
-    ol.innerHTML = ""; 
+    ul.innerHTML = ""; 
        render()
         
     }
